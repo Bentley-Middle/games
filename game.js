a7cc56614a5788cd57f3c24d09e018157192f08c
MrKrasner <78927939+MrKrasner@users.noreply.github.com>
2021-02-11 11:00:58 -0800

    Create game.js

---
+++ game.js
@@ -0,0 +1,37 @@
+const config = {
+     type: Phaser.AUTO,
+     width: 500,
+     height: 300, 
+     physics: {
+    	 default:'arcade',
+    	 arcade: {debug: true}
+    	},
+     scene: {
+         create,
+         update,
+         preload,  
+     }
+}
+const gameState = { 
+     onColor: 0x001001,
+     offColor: 0x000101
+  }
+const game = new Phaser.Game(config)
+
+
+function preload(){
+
+this.load.image('Back', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWBVEjy2EEyE7Y13gWELYcIF_HVLvoDHGMng&usqp=CAU');
+
+}
+
+function create() {
+ //this.add.image(250,150,'Back').setScale(1);
+ gameState.background = this.add.tileSprite(250,150, 500, 300, 'Back');
+ gameState.background.tileScaleX = 1.8;
+}
+
+function update(){
+gameState.background.tilePositionX += 1.5;
+}
+
